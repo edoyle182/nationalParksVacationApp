099509ad618279c051295665057888f345f4b4e7
Sarah Kaplan <sarahkaplan@Sarahs-MBP-2.lan>
2019-09-03 14:51:35 -0400

    minor changes to index.js file

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const NATIONAL_PARKS_URL = 'https://developer.nps.gov/api/v1/';
 const API_KEY = 'jesAgdfOyQGRD6GnNEuCVbkIRSr2h5W9GQikYrMJ';
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params).map(
-    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
-  );
+  const queryItems = Object.keys(params)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
@@ -43,10 +42,10 @@ function getReverseGeocode(latlng, elementID) {
   const splitLatLong = latlng.split(', ');
   const googleLatLng = `latlng=${splitLatLong[0].substr(4)},${splitLatLong[1].substr(5)}`;
   const google_url = 'https://maps.googleapis.com/maps/api/geocode/json?';
-  // const k1 = 'AIzaSyAa6D8GR';
-  // const k2 = '7svXqoZ3K7LPT';
-  // const k3 = 'eQRursUjkmryU'; 
-  const google_api = '&key=' + 'AIzaSyAa6D8GR7svXqoZ3K7LPTeQRursUjkmryU';       // k1 + k3 + k2;
+  // const k1 = 'AIzaSyAymq9T2';
+  // const k2 = 'YaHaY6eI6UCeNv';
+  // const k3 = '9fDk51zVaVXk'; 
+  const google_api = "&key=" + "AIzaSyAymq9T2YaHaY6eI6UCeNv9fDk51zVaVXk";       // k1 + k3 + k2;
   const completeURL = google_url + googleLatLng + google_api;
   console.log(completeURL);
 
@@ -60,9 +59,7 @@ function getReverseGeocode(latlng, elementID) {
     .then(responseJson => {
       if (responseJson.results[0] !== undefined) {
         console.log(responseJson.results[0].formatted_address);
-        $(`#${elementID}`).text(
-          `Address: ${responseJson.results[0].formatted_address}`
-        );
+        $(`#${elementID}`).text(`Address: ${responseJson.results[0].formatted_address}`);
       }
     })
     .catch(err => {
@@ -80,14 +77,15 @@ function displayResults(responseJson) {
     // utilize google reverse geocode API to retrieve address from latitude/longitude coordinates
     let latlng = responseJson.data[obj].latLong;
     let elementID = `li${obj}`;
-    if (latlng !== undefined && latlng !== '') {
+    
+    if (latlng !== undefined && latlng !== "") {
       getReverseGeocode(latlng, elementID);
     }
     // for each object in array, add list item to results
     // list with full name, URL, description, and address (if applicable)
     $('.js-results-list').append(
       `<li><h3><a href="${responseJson.data[obj].url}">${responseJson.data[obj].fullName}</a></h3>
-      <p id='${elementID}'>Address: Not Available</p>
+      <p id='${elementID}'>Address: Not Found</p>
       <p>Description: ${responseJson.data[obj].description}</p>
       </li>
       `
